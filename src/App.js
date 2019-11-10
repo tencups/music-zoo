17ec2764924da4d1cdc7afac678eb45c40e558d3
tencups <26286782+tencups@users.noreply.github.com>
2019-11-10 02:02:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ const Desc = styled("h3")`
   font-size: 3em;
   text-align: center;
   font-family: "Baloo Bhai", cursive;
+color: #000000;
+background: #FFFFFF;
+text-shadow: 2px 2px 0 #bcbcbc, 4px 4px 0 #9c9c9c;
+margin-top: -10px;
 `
 
 const Img = styled("img")`
